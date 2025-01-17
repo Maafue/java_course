b341c63211e822a78a8352f651131cd4d197e5ed
Max Mor <vampir_19.99@mail.ru>
2025-01-17 19:35:32 +0300

    Third commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double number1 = 12;
+        double number2 = -5;
+        double number3 = -6;
+        double number4 = 50;
+
+        double sumOfFourNumbers = number1 + number2 + number3 + number4;
+
+        double result = sumOfFourNumbers / 4;
+
+        System.out.println(result);
+    }
+
+
+}
