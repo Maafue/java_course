1881f6de8e593be57a4285160ca1f125986f820a
Максим <157298229+Maafue@users.noreply.github.com>
2025-01-17 20:45:06 +0300

    Update Tiger.java

--- src/Tiger.java
+++ src/Tiger.java
@@ -1,6 +1,6 @@
 public class Tiger {
-    // private final String name;
-    // private final String lastname;
+    private final String name;
+    private final String lastname;
 
     // public Tiger(String name, String lastname) {
     //     this.name = name;
