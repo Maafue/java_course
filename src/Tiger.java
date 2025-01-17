13b228f5265d8bd21b08781f440187a47146b45c
Max Mor <vampir_19.99@mail.ru>
2025-01-17 20:49:48 +0300

    Comment

--- src/Tiger.java
+++ src/Tiger.java
@@ -2,17 +2,17 @@ public class Tiger {
     private final String name;
     private final String lastname;
 
-    // public Tiger(String name, String lastname) {
-    //     this.name = name;
-    //     this.lastname = lastname;
-    // }
+     public Tiger(String name, String lastname) {
+         this.name = name;
+         this.lastname = lastname;
+     }
 
-    // public static void main(String[] args) {
-    //     Tiger  tiger = new Tiger("Serega", "petrov");
-    //     tiger.print();
-    // }
+     public static void main(String[] args) {
+         Tiger  tiger = new Tiger("Serega", "petrov");
+         tiger.print();
+     }
 
-    // void print () {
-    //     System.out.println("ARRRR I'm " + this.name + " " + this.lastname);
-    // }
+     void print () {
+         System.out.println("ARRRR I'm " + this.name + " " + this.lastname);
+     }
 }
